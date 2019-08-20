fa09250b2a7511e17625699fb4a84379fe58f9cc
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2019-08-20 09:36:44 -0500

    added to index.html, manifest.json, App.js, added new file Form.js, modified Table.js, Modified index.css

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 import React, { Component } from 'react'
 import Table from './Table'
+import Form from './Form'
 
 class App extends Component {
+  state = {
+    characters: []
+  }
+
+  removeCharacter = index => {
+    const { characters } = this.state
+  
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      })
+    })
+  }
+
+  handleSubmit = character => {
+    this.setState({ characters: [...this.state.characters, character] })
+  }
+
   render() {
-    const characters = [
-      {
-        name: 'Charlie',
-        job: 'Janator'
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer'
-      },
-      {
-        name: 'Dee',
-        job: 'Aspiring actress'
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender'
-      }
-    ]
+    const { characters } = this.state
 
     return (
       <div className="App">
-        <Table characterData={ characters }/>
+        <h1>Add Character App</h1>
+        <p>Add a character with a name and a job to the table.</p>
+        <Table characterData={ characters } removeCharacter={ this.removeCharacter }/>
+        <Form handleSubmit={ this.handleSubmit } />
       </div>
     )
   }
