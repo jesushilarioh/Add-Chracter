0f5b78d3d17c37fe633890c060b307cdfab2c2d1
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2019-08-20 00:47:27 -0500

    sent properties to Table component from App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,28 @@ import Table from './Table'
 
 class App extends Component {
   render() {
+    const characters = [
+      {
+        name: 'Charlie',
+        job: 'Janator'
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer'
+      },
+      {
+        name: 'Dee',
+        job: 'Aspiring actress'
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender'
+      }
+    ]
+
     return (
       <div className="App">
-        <Table />
+        <Table characterData={ characters }/>
       </div>
     )
   }
